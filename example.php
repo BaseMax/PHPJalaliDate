49e0045c5b340339bf4809dce735d0d49905b415
Max Base <MaxBaseCode@Gmail.Com>
2022-10-26 20:11:14 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,23 @@
 <?php
 // Max Base
 // https://github.com/BaseMax/PHPJalaliDate
+
 include "src/JalaliDate.php";
+
 ////////////////////////////
-$d=jalaliDate();
-print $d."\n";
+
+print_r(jalaliDate()); // It will return today date in Jalali date system.
+// Array
+// (
+//     [year] => 1401
+//     [month] => 08
+//     [day] => 04
+// )
+
+print_r(jalaliDate(2022, 9, 26));
+// Array
+// (
+//     [year] => 1401
+//     [month] => 07
+//     [day] => 04
+// )
